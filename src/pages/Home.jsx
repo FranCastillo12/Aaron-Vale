c514964f3105cd041a59ad058139fb5a68ae24c3
FranCastillo12 <castillof075@gmail.com>
2026-09-11 23:26:06 -0600

    cambio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 const sobreCerrado = "/images/SOBREDERECHO.png"; 
 const sobreAbierto = "/images/SOBREIZQ.PNG";
-import "../css/home.css";
+import "../css/Home.css";
 
 
 function Home() {
